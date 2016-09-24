agronholm/cbor2

commit 1b84bff4dce4b2f4d86c5848c715cd53a008b12a

Avoid importing modules unless actually used (e.g. uuid, fractions) (#1)

cbor2/encoder.py
@@ -1,15 +1,8 @@
-import math
 import re
 import struct
-from calendar import timegm
 from collections import OrderedDict, Sequence, Mapping
-from datetime import datetime, time, date
-from decimal import Decimal
-from email.message import Message
-from fractions import Fraction
 from functools import wraps
 from io import BytesIO
-from uuid import UUID
 
 from cbor2.compat import iteritems, timezone, long, unicode, as_unicode
 from cbor2.types import CBORTag, undefined
@@ -185,6 +178,7 @@ class CBOREncoder(object):
                     'naive datetime encountered and no default timezone has been set')
 
         if self.datetime_as_timestamp:
+            from calendar import timegm
             timestamp = timegm(value.utctimetuple()) + value.microsecond // 1000000
             self.encode_semantic(1, timestamp, fp)
         else:
@@ -192,6 +186,7 @@ class CBOREncoder(object):
             self.encode_semantic(0, datestring, fp)
 
     def encode_date(self, value, fp):
+        from datetime import datetime, time
         value = datetime.combine(value, time()).replace(tzinfo=timezone.utc)
         self.encode_datetime(value, fp)
 
@@ -232,6 +227,7 @@ class CBOREncoder(object):
 
     def encode_float(self, value, fp):
         # Handle special values efficiently
+        import math
         if math.isnan(value):
             fp.write(b'\xf9\x7e\x00')
         elif math.isinf(value):
@@ -255,7 +251,7 @@ class CBOREncoder(object):
         (int, encode_int),
         (long, encode_int),
         (float, encode_float),
-        (Decimal, encode_decimal),
+        (('decimal', 'Decimal'), encode_decimal),
         (bool, encode_boolean),
         (type(None), encode_none),
         (type(undefined), encode_undefined),
@@ -264,12 +260,12 @@ class CBOREncoder(object):
         (dict, encode_map),
         (Mapping, encode_map),
         (Sequence, encode_array),
-        (datetime, encode_datetime),
-        (date, encode_date),
+        (('datetime', 'datetime'), encode_datetime),
+        (('datetime', 'date'), encode_date),
         (type(re.compile('')), encode_regexp),
-        (Fraction, encode_rational),
-        (Message, encode_mime),
-        (UUID, encode_uuid),
+        (('fractions', 'Fraction'), encode_rational),
+        (('email.message', 'Message'), encode_mime),
+        (('uuid', 'UUID'), encode_uuid),
         (CBORTag, encode_custom_tag)
     ])
 
@@ -283,12 +279,17 @@ class CBOREncoder(object):
 
         """
         obj_type = obj.__class__
-        encoder = self.encoders.get(obj_type)
-        if encoder is None:
+        try:
+            encoder = self.encoders[obj_type]
+        except KeyError:
+            from sys import modules
             # No direct hit -- do a slower subclass check
-            for type_, enc in iteritems(self.encoders):
+            for type_, enc in self.encoders.items():
+                if type(type_) is tuple:
+                    modname, typename = type_
+                    type_ = getattr(modules.get(modname), typename, None)
                 if issubclass(obj_type, type_):
-                    encoder = enc
+                    encoder = self.encoders[obj_type] = enc
                     break
             else:
                 raise CBOREncodeError('cannot serialize type %s' % obj_type.__name__)

cbor2/decoder.py
@@ -1,12 +1,7 @@
 import re
 import struct
 from datetime import datetime, timedelta
-from decimal import Decimal
-from email.parser import Parser
-from fractions import Fraction
 from io import BytesIO
-from math import ldexp
-from uuid import UUID
 
 from cbor2.compat import timezone, xrange, PY2, byte_as_integer
 from cbor2.types import CBORTag, undefined, break_marker
@@ -193,12 +188,14 @@ class CBORDecoder(object):
 
     def decode_fraction(self, value, fp, shareable_index=None):
         # Semantic tag 4
+        from decimal import Decimal
         exp = Decimal(value[0])
         mantissa = Decimal(value[1])
         return mantissa * (10 ** exp)
 
     def decode_bigfloat(self, value, fp, shareable_index=None):
         # Semantic tag 5
+        from decimal import Decimal
         exp = Decimal(value[0])
         mantissa = Decimal(value[1])
         return mantissa * (2 ** exp)
@@ -217,6 +214,7 @@ class CBORDecoder(object):
 
     def decode_rational(self, value, fp, shareable_index=None):
         # Semantic tag 30
+        from fractions import Fraction
         return Fraction(*value)
 
     def decode_regexp(self, value, fp, shareable_index=None):
@@ -225,10 +223,12 @@ class CBORDecoder(object):
 
     def decode_mime(self, value, fp, shareable_index=None):
         # Semantic tag 36
+        from email.parser import Parser
         return Parser().parsestr(value)
 
     def decode_uuid(self, value, fp, shareable_index=None):
         # Semantic tag 37
+        from uuid import UUID
         return UUID(bytes=value)
 
     #
@@ -237,6 +237,8 @@ class CBORDecoder(object):
 
     def decode_float16(self, fp, shareable_index=None):
         # Code adapted from RFC 7049, appendix D
+        from math import ldexp
+
         def decode_single(single):
             return struct.unpack("!f", struct.pack("!I", single))[0]
 

commit 548a21c387b7575fdc76d4e4f71dfc9a6bb327e0

Fixed TypeError when encoding extension types
The bug was introduced by 1b84bff4dce4b2f4d86c5848c715cd53a008b12a.

cbor2/encoder.py
@@ -282,12 +282,16 @@ class CBOREncoder(object):
         try:
             encoder = self.encoders[obj_type]
         except KeyError:
-            from sys import modules
             # No direct hit -- do a slower subclass check
+            from sys import modules
+
             for type_, enc in self.encoders.items():
                 if type(type_) is tuple:
                     modname, typename = type_
                     type_ = getattr(modules.get(modname), typename, None)
+                    if type_ is None:
+                        continue
+
                 if issubclass(obj_type, type_):
                     encoder = self.encoders[obj_type] = enc
                     break

tests/test_encoder.py
@@ -1,5 +1,6 @@
 import re
 from binascii import unhexlify
+from collections import OrderedDict
 from datetime import datetime, timedelta, date
 from decimal import Decimal
 from email.mime.text import MIMEText
@@ -8,7 +9,7 @@ from uuid import UUID
 
 import pytest
 
-from cbor2.compat import timezone
+from cbor2.compat import timezone, unicode
 from cbor2.encoder import dumps, CBOREncodeError, dump
 from cbor2.types import CBORTag, undefined
 
@@ -195,6 +196,17 @@ def test_cyclic_map_nosharing():
 def test_unsupported_type():
     exc = pytest.raises(CBOREncodeError, dumps, lambda: None)
     assert str(exc.value) == 'cannot serialize type function'
+
+
+def test_unimported_type():
+    """Test that the encoder doesn't break when an extension type is not found in sys.modules."""
+    encoders = OrderedDict([
+        (('dummy', 'type'), lambda encoder, obj, fp: None),
+        (('cbor2.encoder', 'CBOREncodeError'),
+         lambda encoder, obj, fp: encoder.encode_string(unicode(obj), fp))
+    ])
+    obj = CBOREncodeError(u'foo')
+    assert dumps(obj, encoders=encoders) == b'\x63foo'
 
 
 def test_custom_encoder():