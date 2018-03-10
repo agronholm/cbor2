ab28c7a8fbc0507ce09def1e240fc0e2a4cfd793
Pulkit Goyal <7895pulkit@gmail.com>
2018-03-10 12:38:53 +0530

    cbor2: add `from __future__ import absolute_import` to files

--- cbor2/__init__.py
+++ cbor2/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from cbor2.decoder import load, loads, CBORDecoder, CBORDecodeError  # noqa
 from cbor2.encoder import dump, dumps, CBOREncoder, CBOREncodeError, shareable_encoder  # noqa
 from cbor2.types import CBORTag, CBORSimpleValue, undefined  # noqa
