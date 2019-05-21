173cc15799b5fc187df43bab74134b89c3604621
Dave Jones <dave@waveform.org.uk>
2019-05-21 10:45:32 +0100

    Merge cboar into cbor2 as _cbor2 module

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-from setuptools import setup
+import sys
+import platform
+from setuptools import setup, Extension
+
+
+if platform.python_implementation() == 'CPython' and sys.version_info >= (3, 3):
+    _cbor2 = Extension(
+        '_cbor2',
+        libraries=['m'],
+        sources=[
+            'source/module.c',
+            'source/encoder.c',
+            'source/decoder.c',
+            'source/tags.c',
+            'source/halffloat.c',
+        ]
+    )
+    kwargs = {'ext_modules': [_cbor2]}
+else:
+    kwargs = {}
+
 
 setup(
     use_scm_version={
@@ -8,5 +28,6 @@ setup(
     setup_requires=[
         'setuptools >= 36.2.7',
         'setuptools_scm >= 1.7.0'
-    ]
+    ],
+    **kwargs
 )
