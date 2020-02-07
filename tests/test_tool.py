0afec2c7bee3da879a80b9b1e136767f44dfd284
Kio Smallwood <kio.smallwood@flexitricity.com>
2020-02-07 18:56:08 +0000

    Further tests of streaming and handling bytes

--- tests/test_tool.py
+++ tests/test_tool.py
@@ -2,7 +2,6 @@ import pytest
 import sys
 import binascii
 import base64
-from unittest import mock
 from io import StringIO, BytesIO, TextIOWrapper
 
 import cbor2.tool
@@ -28,4 +27,34 @@ def test_readfrom(monkeypatch, tmpdir):
         m.setattr('sys.argv', [''] + argv)
         cbor2.tool.main()
         assert outfile.read() == '2\n'
+
+def test_b64(monkeypatch, tmpdir):
+    f = tmpdir.join('outfile')
+    argv = ['-d', '-o', str(f)]
+    inbuf = TextIOWrapper(BytesIO(b'oQID'))
+    with monkeypatch.context() as m:
+        m.setattr('sys.argv', [''] + argv)
+        m.setattr('sys.stdin', inbuf)
+        cbor2.tool.main()
+        assert f.read() == '{"2": 3}\n'
+
+def test_stream(monkeypatch, tmpdir):
+    f = tmpdir.join('outfile')
+    argv = ['--sequence', '-o', str(f)]
+    inbuf = TextIOWrapper(BytesIO(binascii.unhexlify('0203')))
+    with monkeypatch.context() as m:
+        m.setattr('sys.argv', [''] + argv)
+        m.setattr('sys.stdin', inbuf)
+        cbor2.tool.main()
+        assert f.read() == '2\n3\n'
+
+def test_embed_bytes(monkeypatch, tmpdir):
+    f = tmpdir.join('outfile')
+    argv = [ '-o', str(f)]
+    inbuf = TextIOWrapper(BytesIO(binascii.unhexlify('457374756666')))
+    with monkeypatch.context() as m:
+        m.setattr('sys.argv', [''] + argv)
+        m.setattr('sys.stdin', inbuf)
+        cbor2.tool.main()
+        assert f.read() == '"c3R1ZmY="\n'
 
