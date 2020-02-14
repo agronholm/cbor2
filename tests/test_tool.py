6cd8df88bb305e6789258e9f33024dd16178cb2f
Kio Smallwood <kio.smallwood@flexitricity.com>
2020-02-14 13:42:09 +0000

    Fix flake8 nits and extend skip tests for py35 without C lib

--- tests/test_tool.py
+++ tests/test_tool.py
@@ -1,7 +1,6 @@
 import pytest
 import sys
 import binascii
-import base64
 from io import BytesIO, TextIOWrapper
 
 import cbor2.tool
@@ -28,6 +27,7 @@ def test_readfrom(monkeypatch, tmpdir):
         cbor2.tool.main()
         assert outfile.read() == '2\n'
 
+
 def test_b64(monkeypatch, tmpdir):
     f = tmpdir.join('outfile')
     argv = ['-d', '-o', str(f)]
@@ -37,6 +37,7 @@ def test_b64(monkeypatch, tmpdir):
         m.setattr('sys.stdin', inbuf)
         cbor2.tool.main()
         assert f.read() == '{"2": 3}\n'
+
 
 def test_stream(monkeypatch, tmpdir):
     f = tmpdir.join('outfile')
@@ -48,9 +49,10 @@ def test_stream(monkeypatch, tmpdir):
         cbor2.tool.main()
         assert f.read() == '2\n3\n'
 
+
 def test_embed_bytes(monkeypatch, tmpdir):
     f = tmpdir.join('outfile')
-    argv = [ '-o', str(f)]
+    argv = ['-o', str(f)]
     inbuf = TextIOWrapper(BytesIO(binascii.unhexlify('43010203')))
     expected = '"AQID"\n' if sys.version_info >= (3, 3) else '"\u0001\u0002\u0003"\n'
     with monkeypatch.context() as m:
@@ -59,13 +61,13 @@ def test_embed_bytes(monkeypatch, tmpdir):
         cbor2.tool.main()
         assert f.read() == expected
 
-@pytest.mark.skipif(sys.version_info < (3, 3), reason="No ipaddress module")
+
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="No ipaddress module and simple value is unhashable")
 def test_dtypes_from_file(monkeypatch, tmpdir):
     infile = 'tests/examples.cbor.b64'
     outfile = tmpdir.join('outfile.json')
-    argv = [ '--sort-keys', '-d', '-o', str(outfile), infile ]
+    argv = ['--sort-keys', '-d', '-o', str(outfile), infile]
     with monkeypatch.context() as m:
         m.setattr('sys.argv', [''] + argv)
         cbor2.tool.main()
         assert outfile.read().startswith('{"bytes": [')
-
