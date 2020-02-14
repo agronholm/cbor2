20c02ed9a83f092ed2bc61d037f410a3fd148ad8
Kio Smallwood <kio.smallwood@flexitricity.com>
2020-02-14 11:14:33 +0000

    Pre-process map keys to make them palatable for json

--- cbor2/tool.py
+++ cbor2/tool.py
@@ -3,17 +3,24 @@ import json
 import sys
 import base64
 import io
+import re
 from datetime import datetime
 from collections import OrderedDict
 from . import load, CBORDecoder
-from .types import *
+from .types import FrozenDict, CBORTag, undefined, CBORSimpleValue
 
 import decimal, fractions, uuid, ipaddress
+
+try:
+    from _cbor2 import CBORTag, undefined, CBORSimpleValue
+except ImportError:
+    pass
 
 default_encoders = OrderedDict([
     (bytes,                         lambda x: base64.b64encode(x).decode('ascii')),
     (decimal.Decimal,               lambda x: str(x)),
     (FrozenDict,                    lambda x: str(dict(x))),
+    (CBORSimpleValue,               lambda x: 'cbor_simple:{:d}'.format(x.value)),
     (type(undefined),               lambda x: 'cbor:undef'),
     (datetime,                      lambda x: x.isoformat()),
     (fractions.Fraction,            lambda x: str(x)),
@@ -22,10 +29,9 @@ default_encoders = OrderedDict([
     (ipaddress.IPv6Address,  lambda x: str(x)),
     (ipaddress.IPv4Network,  lambda x: str(x)),
     (ipaddress.IPv6Network,  lambda x: str(x)),
-    (CBORSimpleValue,               lambda x: "cbor_simple:{:d}".format(x.value)),
-    (CBORTag,                       lambda x: 'cbor_tag:{:d}:{}'.format( x.tag, x.value )),
+    (CBORTag,                       lambda x: { 'CBORTag:{:d}'.format(x.tag):  x.value }),
     (set,                           lambda x: list(x)),
-    (frozenset,                     lambda x: str(x)),
+    (re.compile('').__class__,                     lambda x: str(x)),
 ])
 
 class DefEncoder(json.JSONEncoder):
@@ -34,8 +40,6 @@ class DefEncoder(json.JSONEncoder):
         encoder = default_encoders.get(obj_type)
         if encoder:
             return encoder(v)
-        else:
-            return str(v)
         return json.JSONEncoder.default(self, v)
 
 def iterdecode(f):
@@ -46,6 +50,31 @@ def iterdecode(f):
         except EOFError:
             return
 
+def key_to_str(d):
+    rval = {}
+    if not isinstance(d, dict):
+        if isinstance(d, CBORSimpleValue):
+            v = 'cbor_simple:{:d}'.format(d.value)
+            return v
+        if isinstance(d,(tuple,list,set)):
+            v = [key_to_str(x) for x in d]
+            return v
+        else:
+            return d
+
+    for k,v in d.items():
+        if isinstance(k,bytes):
+            k = base64.b64encode(k).decode('ascii')
+        if isinstance(k, CBORSimpleValue):
+            k = 'cbor_simple:{:d}'.format(k.value)
+        if isinstance(k, (FrozenDict, frozenset, tuple)):
+            k = str(k)
+        if isinstance(v,dict):
+            v = key_to_str(v)
+        elif isinstance(v,(tuple,list,set)):
+            v = [key_to_str(x) for x in v]
+        rval[k] = v
+    return rval
 
 def main():
     prog = 'python -m cbor2.tool'
@@ -86,7 +115,7 @@ def main():
                     else:
                         objs = (load(infile), )
                     for obj in objs:
-                        json.dump(obj, outfile, sort_keys=sort_keys, indent=(None, 4)[pretty], cls=DefEncoder)
+                        json.dump(key_to_str(obj), outfile, sort_keys=sort_keys, indent=(None, 4)[pretty], cls=DefEncoder)
                         outfile.write('\n')
                 except ValueError as e:
                     raise SystemExit(e)
