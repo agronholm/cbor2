36c908731e54e2d7fa6995d839aba508084d51a0
Kio Smallwood <kio.smallwood@flexitricity.com>
2020-02-19 12:33:32 +0000

    Fix flake8 nits

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -210,6 +210,7 @@ def test_simple_val_as_key(impl):
     payload = {impl.CBORSimpleValue(99): 1}
     result = impl.dumps(payload)
     assert result == unhexlify('A1F86301')
+
 #
 # Tests for extension tags
 #
