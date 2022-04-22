2ac3a7532ea46b494361f8ebd292270a96806c4c
Samuel M Smith <sam@samuelsmith.org>
2022-04-22 15:39:31 -0600

    removed faulty scm versioning

--- setup.py
+++ setup.py
@@ -65,10 +65,10 @@ else:
 
 
 setup(
-    use_scm_version={
-        'version_scheme': 'post-release',
-        'local_scheme': 'dirty-tag'
-    },
+#    use_scm_version={
+#        'version_scheme': 'post-release',
+#        'local_scheme': 'dirty-tag'
+#    },
     setup_requires=[
         'setuptools >= 40.7.0',
         'setuptools_scm >= 1.7.0'
