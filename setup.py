2b38579343a9fdc499bdb1d17a45ea5be65f89e0
Kio Smallwood <kio.smallwood@flexitricity.com>
2022-05-03 14:22:03 +0100

    Fix wheel builds from master
    This lets us use X.y.x.postN git tags in the case where we do a build-system update

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ else:
 
 
 setup(
-    use_scm_version={"version_scheme": "post-release", "local_scheme": "dirty-tag"},
+    use_scm_version={"version_scheme": "guess-next-dev", "local_scheme": "dirty-tag"},
     setup_requires=["setuptools >= 61", "setuptools_scm >= 6.4"],
     **kwargs
 )
