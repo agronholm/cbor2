88e4b9008d45682ee1db2d9e9c3574bb3c23aabe
Matt Schwager <matt.schwager@trailofbits.com>
2024-04-23 08:11:57 -0400

    Move OSS-Fuzz target file under tests

---
+++ tests/fuzzers/loads_fuzzer.py
@@ -0,0 +1,22 @@
+import sys
+import atheris
+
+# _cbor2 ensures the C library is imported
+from _cbor2 import loads
+
+
+def test_one_input(data: bytes):
+    try:
+        loads(data)
+    except Exception:
+        # We're searching for memory corruption, not Python exceptions
+        pass
+
+
+def main():
+    atheris.Setup(sys.argv, test_one_input)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
